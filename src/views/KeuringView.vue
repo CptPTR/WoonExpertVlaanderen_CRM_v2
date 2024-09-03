98a624fe287ee7c12be4993bf42ce6c978d34e56
Peter De Clercq <peterdeclercq1997@outlook.com>
2024-09-03 08:58:53 +0200

    💡 Fix later: recent uploaded certificate

--- src/views/KeuringView.vue
+++ src/views/KeuringView.vue
@@ -297,7 +297,7 @@
                 <div>
                   <span class="naam text-sm">
                     {{ certificaat.naam }}
-                    <p v-if="isLessThanOneDayOld(certificaat.created_at)" class="recent">Recent geüpload</p>
+                    <!-- <p v-if="isLessThanOneDayOld(certificaat.created_at)" class="recent">Recent geüpload</p> -->
                   </span>
                   <span class="size text-xs"> ({{ formatFileSize(certificaat.size) }}) </span>
                 </div>
